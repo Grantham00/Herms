4eca999e652a7ccb713d9eb4dd7bf259bdbb52f9
ant <grantham00@gmail.com>
2020-03-18 12:54:30 -0400

    adjust telem

--- listen_odom_imu.py
+++ listen_odom_imu.py
@@ -17,24 +17,33 @@ import serial
 
 
 def serial_listen():
-    imu_in = ser.readline().decode('ASCII')
-    imu_in = imu_in.split('-')
-    quaternion = imu_in[0][2:-1].split(',')
-    quaternion = [float(i) for i in quaternion]
-    quaternion.append(quaternion.pop(0))
-    gyro = imu_in[1][1:-1].split(',')
-    gyro = [float(i) for i in gyro]
-    accel = imu_in[2][1:-3].split(',')
-    accel = [float(i) for i in accel]
+    empty = True
+    while empty:
+        try:
+            imu_in = ser.readline().decode('ASCII')
+            imu_in = imu_in.split('-')
+            quaternion = imu_in[0][2:-1].split(',')
+            quaternion = [float(i) for i in quaternion]
+            quaternion.append(quaternion.pop(0))
+            gyro = imu_in[1][1:-1].split(',')
+            gyro = [float(i) for i in gyro]
+            accel = imu_in[2][1:-1].split(',')
+            accel = [float(i) for i in accel]
+            
+            encoder_vel_left = float(imu_in[3])
+            encoder_vel_right =float(imu_in[4][0:-2])
+            empty = False
+        except:
+            pass
     
-    return quaternion, gyro, accel
+    return quaternion, gyro, accel, encoder_vel_left, encoder_vel_right
 
 def talker():
     pub = rospy.Publisher('Imu', Imu, queue_size=1)
     rospy.init_node('LISTEN_ODOM_IMU')
     rate = rospy.Rate(30)
     while not rospy.is_shutdown():
-        quaternion, gyro, accel = serial_listen()
+        quaternion, gyro, accel, encoder_vel_left, encoder_vel_right = serial_listen()
         Imu.orientation = quaternion
         Imu.angular_velocity = gyro
         Imu.linear_acceleration = accel
